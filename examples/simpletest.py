f136b2f766e1102812cebd4e17ee7dee85fcb169
Tony DiCola <tony@tonydicola.com>
2016-04-19 09:28:08 +0000

    Initial commit.

---
+++ examples/simpletest.py
@@ -0,0 +1,28 @@
+# Simple demo of of the LSM303 accelerometer & magnetometer library.
+# Will print the accelerometer & magnetometer X, Y, Z axis values every half
+# second.
+# Author: Tony DiCola
+# License: Public Domain
+import time
+
+# Import the LSM303 module.
+import Adafruit_LSM303
+
+
+# Create a LSM303 instance.
+lsm303 = Adafruit_LSM303.LSM303()
+
+# Alternatively you can specify the I2C bus with a bus parameter:
+#lsm303 = Adafruit_LSM303.LSM303(bus=2)
+
+print('Printing accelerometer & magnetometer X, Y, Z axis values, press Ctrl-C to quit...')
+while True:
+    # Read the X, Y, Z axis acceleration values and print them.
+    accel, mag = lsm303.read()
+    # Grab the X, Y, Z components from the reading and print them out.
+    accel_x, accel_y, accel_z = accel
+    mag_x, mag_y, mag_z = mag
+    print('Accel X={0}, Accel Y={1}, Accel Z={2}, Mag X={3}, Mag Y={4}, Mag Z={5}'.format(
+          accel_x, accel_y, accel_z, mag_x, mag_y, mag_z))
+    # Wait half a second and repeat.
+    time.sleep(0.5)
